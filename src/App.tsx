9a0631e94f30afa2ace84287c506322484917dde
Philip Han <46556419+Philipnah@users.noreply.github.com>
2025-06-05 13:43:36 +0200

    testing with reactbits background

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import Chat from "./components/Chat";
 
+import Silk from "./blocks/Backgrounds/Silk/Silk";
+
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen gap-4">
-      <h1 className="text-4xl font-bold">Welcome to PinIT</h1>
-      <h2 className="text-lg px-4 text-center">
-        Pin any ITU related news to this page to share it among the wonderful
-        students of ITU! 🥳
-      </h2>
-      <Chat />
-    </div>
+    <>
+      <div className="fixed inset-0 w-full h-full -z-10">
+        <Silk
+          speed={5}
+          scale={1}
+          color="#7B7481"
+          noiseIntensity={1.5}
+          rotation={0}
+        />
+      </div>
+      <div className="flex flex-col items-center justify-center h-screen gap-4">
+        <h1 className="text-4xl font-bold">Welcome to PinIT</h1>
+        <h2 className="text-lg px-4 text-center">
+          Pin any ITU related news to this page to share it among the wonderful
+          students of ITU! 🥳
+        </h2>
+        <Chat />
+      </div>
+    </>
   );
 }
 
