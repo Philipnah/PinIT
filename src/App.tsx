efa8b5371c28c9deaec48e5c3a1bb0ee1fd3d0f5
Philip Han <46556419+Philipnah@users.noreply.github.com>
2025-06-08 00:28:48 +0200

    add vercel analytics component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Chat from "./components/Chat";
 import Title from "./components/Title";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center h-screen gap-4">
+      <Analytics />
       <Title text="Welcome to PinIT📌" />
       <h2 className="text-lg px-4 text-center">
         Pin any ITU related news to this page to share it among the wonderful
