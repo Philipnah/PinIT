e226da1bb03516ae241963c5d0907f646b171a5b
Philip Han <46556419+Philipnah@users.noreply.github.com>
2025-06-15 21:51:31 +0200

    refactor InputField to be a form

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,32 +1,49 @@
-import { useState } from "react";
+import { useState, type FormEvent } from "react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import supabase from "@/utils/supabase";
 
 export default function InputField() {
   const [inputField, setInputField] = useState<string>("");
 
+  function handleOnSubmit(event: FormEvent) {
+    event.preventDefault();
+    if (inputField == "") return;
+
+    handlePinButtonClick(inputField);
+    setInputField("");
+  }
+
+  async function handlePinButtonClick(text: string) {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    console.log(user?.email + " wanted to pin: " + text);
+  }
+
   return (
-    <footer className="fixed bottom-0 left-0 right-0 p-4">
+    <form
+      className="fixed bottom-0 left-0 right-0 p-4"
+      onSubmit={(event) => handleOnSubmit(event)}
+    >
       <div className="mx-auto max-w-2xl bg-white rounded-lg shadow-lg border p-4">
         <div className="flex flex-row items-center justify-center gap-4">
           <Input
             placeholder="Type something to pin here..."
+            type="text"
+            value={inputField}
             className="w-full transition-all duration-200 hover:-translate-y-1 hover:shadow-lg focus:-translate-y-1 focus:shadow-lg"
             onChange={(event) => setInputField(event.target.value)}
           />
           <Button
             type="submit"
-            onClick={() => handlePinButtonClick(inputField)}
             className="transition-all duration-200 hover:-translate-y-1 hover:shadow-lg active:translate-y-0 active:shadow-sm"
           >
             Pin 📌
           </Button>
         </div>
       </div>
-    </footer>
+    </form>
   );
-}
-
-function handlePinButtonClick(text: string) {
-  console.log("User wanted to pin: " + text);
 }
